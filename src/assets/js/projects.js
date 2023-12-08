325918e9053e8209226c42f695699a34336d6e81
Lorenzo-Olivar <131014446+Lorenzo-Olivar@users.noreply.github.com>
2023-12-08 08:39:21 -0500

    changed where the pictures are

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -5,7 +5,7 @@ const projects = [
         projectName: `Lorenzo's Dev Portfolio`,
         shortDescription: `This was my previous portfolio that I hope to recreate with React`,
         fullDescription: `My motivation for making a portfolio webpage is to show my growth as a new developer to the community of developers and future employers. I want my portfolio to showcase my knowledge and the rate to pick up a new skill or trend within the dev community and workplace. I want to tell a story about my passion and willingness to achieve new heights as a developer and for my future workplace peers to be confident in my developer and team player abilities.`,
-        picture: 'src/assets/images/Screenshot 2023-12-08 063843.png',
+        picture: 'public/Screenshot 2023-12-08 063843.png',
         link: {
             website: 'https://lorenzo-olivar.github.io/Lorenzo-s-Dev-Portfolio/',
             gitHub: 'https://github.com/Lorenzo-Olivar/Lorenzo-s-Dev-Portfolio',
@@ -17,7 +17,7 @@ const projects = [
         projectName: `JATE by Zo`,
         shortDescription: `My first client server fullstack application!`,
         fullDescription: `Just Another Text Editor (J.A.T.E) is a text editor that stores code snippets or texts you might keep for yourself. This project helped me practice making full-stack applications as installable apps. Jate helps me store code that I plan to use in all my projects so I can work smarter and not harder! From this project, I learned how to structure my code into a server and client format while learning how to cache my application so I can use the app without the internet.`,
-        picture: 'src/assets/images/Jate by Zo.png',
+        picture: 'public/Jate by Zo.png',
         link: {
             website: 'https://jate-by-zo-olivar.onrender.com/',
             gitHub: 'https://github.com/Lorenzo-Olivar/jate-by-zo',
@@ -32,7 +32,7 @@ const projects = [
         I built this project to familiarize myself with the use of third-party APIs and their functions.
         Thankfully, this scheduler helps with organizing an individual's busy life by assigning events and things to do at a specific time.
         Overall, I learned how beneficial it is to utilize third-party APIs to be more efficient when coding.`,
-        picture: 'src/assets/images/Block Scheduler.png',
+        picture: 'public/Block Scheduler.png',
         link: {
             website: 'https://lorenzo-olivar.github.io/Block-Scheduler/',
             gitHub: 'https://github.com/Lorenzo-Olivar/Block-Scheduler',
@@ -46,7 +46,7 @@ const projects = [
         fullDescription: `As a Developer,
         I WANT to create a quiz about JavaScript code,
         So that I can quiz the user on their knowledge of JavaScript.`,
-        picture: 'src/assets/images/JavaScript Quiz.png',
+        picture: 'public/JavaScript Quiz.png',
         link: {
             website: 'https://lorenzo-olivar.github.io/Zo-JavaScript-Quiz/',
             gitHub: 'https://github.com/Lorenzo-Olivar/Zo-JavaScript-Quiz',
